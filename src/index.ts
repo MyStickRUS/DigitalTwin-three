4bbc88f0549a140942994ccfba1243892e8fb92b
SergeiA <sergeia@dev.com>
2023-05-21 23:27:48 +0400

    TODO: initial cam position

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { Font, FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 let controls: OrbitControls;
 let scene: THREE.Scene;
@@ -12,21 +13,46 @@ let mouse: THREE.Vector2;
 let tooltipMesh: THREE.Mesh | null = null;
 let loadedFont: Font | null = null;
 
+let clickTimeout: number | null = null;
+let mouseDown = false;
+let pointerMovedSinceMouseDown = false;
+
 let cameraTargetTo: THREE.Vector3 | null = null;
 
-const lerpSpeed = 0.05; // camera transition speed
+const lerpSpeed = 0.05; // smooth transition speed
 
-const loader = new FontLoader();
+const fontLoader = new FontLoader();
+const modelLoader = new GLTFLoader();
 
-const objects: THREE.Mesh[] = [];
+const objects: (THREE.Mesh | THREE.Group)[] = [];
 
-loader.load('node_modules/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+fontLoader.load('node_modules/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
   loadedFont = font
   init();
   animate();
 });
 
 window.addEventListener('pointermove', onPointerMove);
+window.addEventListener('mousedown', onMouseDown);
+window.addEventListener('mousemove', onMouseMove);
+window.addEventListener('mouseup', onMouseUp);
+window.addEventListener("click", onMouseClick, false);
+window.addEventListener("dblclick", onMouseDoubleClick, false);
+
+function onMouseDown(event: MouseEvent) {
+  mouseDown = true;
+  pointerMovedSinceMouseDown = false;
+}
+
+function onMouseMove(event: MouseEvent) {
+  if (mouseDown) {
+    pointerMovedSinceMouseDown = true;
+  }
+}
+
+function onMouseUp(event: MouseEvent) {
+  mouseDown = false;
+}
 
 function init() {
   scene = new THREE.Scene();
@@ -36,7 +62,7 @@ function init() {
     0.1,
     1000
   );
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  // camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   renderer = new THREE.WebGLRenderer();
 
@@ -44,18 +70,17 @@ function init() {
   document.body.appendChild(renderer.domElement);
 
   addPlane();
-
+  addTruck();
   addBoxes();
 
   raycaster = new THREE.Raycaster();
   mouse = new THREE.Vector2();
 
   controls = new OrbitControls(camera, renderer.domElement);
+  controls.autoRotate = true; 
   controls.target.set(0, 0, 0);
   camera.rotateX(0.5);
-
-  window.addEventListener("click", onMouseClick, false);
-  window.addEventListener("dblclick", onMouseDoubleClick, false);
+  controls.update();
 
   camera.position.z = 5;
 }
@@ -71,14 +96,25 @@ function addPlane() {
   scene.add(plane);
 }
 
+function addTruck() {
+  return modelLoader.load('cyber.glb', function (gltf) {
+    const modelGroup = new THREE.Group();
+    modelGroup.add(gltf.scene);
+
+    scene.add(modelGroup);
+    objects.push(modelGroup);
+
+    modelGroup.position.set(0, -0.4, -10);
+  },
+    undefined,
+    function (error) {
+      console.error(error);
+    });
+}
+
 function addBoxes() {
   const boxGeometry = new THREE.BoxGeometry();
   const boxMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
-
-  const box1 = new THREE.Mesh(boxGeometry, boxMaterial);
-  box1.position.set(-2, 0.5, 0);
-  scene.add(box1);
-  objects.push(box1);
 
   const box2 = new THREE.Mesh(boxGeometry, boxMaterial);
   box2.position.set(2, 0.5, 0);
@@ -87,6 +123,10 @@ function addBoxes() {
 }
 
 function onMouseDoubleClick(event: MouseEvent) {
+  if (clickTimeout) {
+    clearTimeout(clickTimeout);
+  }
+
   raycaster.setFromCamera(mouse, camera);
 
   const intersects = raycaster.intersectObjects(objects);
@@ -99,18 +139,24 @@ function onMouseDoubleClick(event: MouseEvent) {
 }
 
 function onMouseClick(event: MouseEvent) {
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(objects);
-
-  if (intersects.length) {
-    const firstIntersection = intersects[0];
-    createTooltip('Tooltip text', firstIntersection.object.position);
-
+  if (pointerMovedSinceMouseDown) {
     return;
   }
 
-  tooltipMesh && scene.remove(tooltipMesh);
+  clickTimeout = setTimeout(() => {
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(objects);
+
+    if (intersects.length) {
+      const firstIntersection = intersects[0];
+      createTooltip('Tooltip text', firstIntersection.object.position);
+
+      return;
+    }
+
+    tooltipMesh && scene.remove(tooltipMesh);
+  }, 250)
 }
 
 function onPointerMove(event: PointerEvent) {
@@ -118,7 +164,8 @@ function onPointerMove(event: PointerEvent) {
   mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
 
-function createTooltip(text: string, position: THREE.Vector3) {
+function createTooltip(text: string, position: any) {
+  // FIXME: position is THREE.Vector3 | THREE.Group
   if (!loadedFont) {
     return console.error("No font detected!")
   }
@@ -132,6 +179,21 @@ function createTooltip(text: string, position: THREE.Vector3) {
   });
 
   tooltipMesh = new THREE.Mesh(geometry, material);
+
+  if (position?.object) {
+    // object is a group
+    const box = new THREE.Box3().setFromObject(position.object);
+    const boxCenter = box.getCenter(new THREE.Vector3());
+    const boxSize = box.getSize(new THREE.Vector3());
+
+    tooltipMesh.position.copy(boxCenter);
+    tooltipMesh.position.y += boxSize.y / 2 + 0.5; // position it above the bounding box
+
+    scene.add(tooltipMesh);
+    return;
+  }
+
+  // object is a mesh 
   tooltipMesh.position.copy(position);
   tooltipMesh.position.y += 1;
 
@@ -153,12 +215,6 @@ function createTooltip(text: string, position: THREE.Vector3) {
 function animateLerp() {
   if (cameraTargetTo !== null) {
     controls.target.lerp(cameraTargetTo, lerpSpeed);
-
-    // Stop the lerp if the target is close enough
-    if (controls.target.distanceTo(cameraTargetTo) < 0.01) {
-      controls.target.copy(cameraTargetTo);
-      cameraTargetTo = null;
-    }
 
     controls.update();
   }
