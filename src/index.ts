cde8920e5d5787fab192e38097d11f140c301738
SergeiA <sergeia@dev.com>
2023-05-21 22:14:58 +0400

    Tooltips

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { Font, FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 
 let controls: OrbitControls;
 let scene: THREE.Scene;
@@ -7,15 +9,22 @@ let camera: THREE.PerspectiveCamera
 let renderer: THREE.WebGLRenderer;
 let raycaster: THREE.Raycaster
 let mouse: THREE.Vector2;
+let tooltipMesh: THREE.Mesh | null = null;
+let loadedFont: Font | null = null;
 
 let cameraTargetTo: THREE.Vector3 | null = null;
 
 const lerpSpeed = 0.05; // camera transition speed
 
+const loader = new FontLoader();
+
 const objects: THREE.Mesh[] = [];
 
-init();
-animate();
+loader.load('node_modules/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+  loadedFont = font
+  init();
+  animate();
+});
 
 window.addEventListener('pointermove', onPointerMove);
 
@@ -34,17 +43,9 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  addBoxes();
+  addPlane();
 
-  // Add a plane
-  const planeGeometry = new THREE.PlaneGeometry(15, 15);
-  const planeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
-    side: THREE.DoubleSide
-  });
-  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.rotation.x = Math.PI / 2;
-  scene.add(plane);
+  addBoxes();
 
   raycaster = new THREE.Raycaster();
   mouse = new THREE.Vector2();
@@ -54,13 +55,25 @@ function init() {
   camera.rotateX(0.5);
 
   window.addEventListener("click", onMouseClick, false);
+  window.addEventListener("dblclick", onMouseDoubleClick, false);
 
   camera.position.z = 5;
 }
 
+function addPlane() {
+  const planeGeometry = new THREE.PlaneGeometry(15, 15);
+  const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 'grey',
+    side: THREE.DoubleSide
+  });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = Math.PI / 2;
+  scene.add(plane);
+}
+
 function addBoxes() {
   const boxGeometry = new THREE.BoxGeometry();
-  const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const boxMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
 
   const box1 = new THREE.Mesh(boxGeometry, boxMaterial);
   box1.position.set(-2, 0.5, 0);
@@ -73,7 +86,7 @@ function addBoxes() {
   objects.push(box2);
 }
 
-function onMouseClick(event: any) {
+function onMouseDoubleClick(event: MouseEvent) {
   raycaster.setFromCamera(mouse, camera);
 
   const intersects = raycaster.intersectObjects(objects);
@@ -85,14 +98,59 @@ function onMouseClick(event: any) {
   }
 }
 
+function onMouseClick(event: MouseEvent) {
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(objects);
+
+  if (intersects.length) {
+    const firstIntersection = intersects[0];
+    createTooltip('Tooltip text', firstIntersection.object.position);
+
+    return;
+  }
+
+  tooltipMesh && scene.remove(tooltipMesh);
+}
+
 function onPointerMove(event: PointerEvent) {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+function createTooltip(text: string, position: THREE.Vector3) {
+  if (!loadedFont) {
+    return console.error("No font detected!")
+  }
 
+  if (tooltipMesh) scene.remove(tooltipMesh); // remove old tooltip if exists
+
+  const geometry = new THREE.PlaneGeometry(1, 0.5);
+  const material = new THREE.MeshBasicMaterial({
+    color: 'blue',
+    side: THREE.DoubleSide
+  });
+
+  tooltipMesh = new THREE.Mesh(geometry, material);
+  tooltipMesh.position.copy(position);
+  tooltipMesh.position.y += 1;
+
+  const textGeometry = new TextGeometry(text, {
+    font: loadedFont,
+    size: 0.1,
+    height: 0.01,
+  });
+
+  const textMaterial = new THREE.MeshBasicMaterial({ color: 'white' });
+  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+  textMesh.position.set(-0.5, 0, 0);
+
+  tooltipMesh.add(textMesh);
+
+  scene.add(tooltipMesh);
+}
+
+function animateLerp() {
   if (cameraTargetTo !== null) {
     controls.target.lerp(cameraTargetTo, lerpSpeed);
 
@@ -104,6 +162,11 @@ function animate() {
 
     controls.update();
   }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  animateLerp();
 
   renderer.render(scene, camera);
 }
